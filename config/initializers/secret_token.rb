7361ea91d6d9e8b8735b84d331a6086335b7cc7b
Swaroop SM <swaroop.striker@gmail.com>
2013-08-11 11:34:23 +0530

    initialized project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Colloquy::Application.config.secret_token = '3f1b5260167ed135d22e12d0974346a2044e3ab96a1fddf963dc7f1769a27afd3c1119ccc766980e4be5d455d892705cdda1fce1351908f9052cdfa96ee16b6a'
