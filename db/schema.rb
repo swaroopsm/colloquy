e8a9756d1438a05f8bf09a4d9032736ab5c090b0
Swaroop SM <swaroop.striker@gmail.com>
2013-08-11 16:58:22 +0530

    ran migrations after pull and merged from fizz

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,17 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130811110330) do
+ActiveRecord::Schema.define(:version => 20130811111326) do
+
+  create_table "conferences", :force => true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.date     "start_date"
+    t.integer  "number_of_days"
+    t.string   "venue"
+    t.boolean  "active"
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
+  end
 
 end
