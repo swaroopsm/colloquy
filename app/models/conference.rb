08ca9a5377fea2f8f384c5ff2b2ac63455790f0f
gonecase <arjuna.shankar@gmail.com>
2013-08-11 17:01:10 +0530

    added conference and user associations

--- app/models/conference.rb
+++ app/models/conference.rb
@@ -1,3 +1,5 @@
 class Conference < ActiveRecord::Base
   attr_accessible :active, :description, :number_of_days, :start_date, :title, :venue
+
+  has_many :users, :class_name => "ConferenceUser"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,42 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130811110330) do
+ActiveRecord::Schema.define(:version => 20130811112840) do
+
+  create_table "conference_users", :force => true do |t|
+    t.integer  "conference_id"
+    t.string   "user_id"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
+  end
+
+  create_table "conferences", :force => true do |t|
+    t.string   "title"
+    t.text     "description"
+    t.date     "start_date"
+    t.integer  "number_of_days"
+    t.string   "venue"
+    t.boolean  "active"
+    t.datetime "created_at",     :null => false
+    t.datetime "updated_at",     :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "email",                  :default => "", :null => false
+    t.string   "encrypted_password",     :default => "", :null => false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          :default => 0
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                             :null => false
+    t.datetime "updated_at",                             :null => false
+  end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
+  add_index "users", ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
 
 end
