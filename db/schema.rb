64ab20d55bed55e447529493ec48bb9c8319b166
gonecase <arjuna.shankar@gmail.com>
2013-08-11 17:06:36 +0530

    added roles model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130811112840) do
+ActiveRecord::Schema.define(:version => 20130811113531) do
 
   create_table "conference_users", :force => true do |t|
     t.integer  "conference_id"
@@ -29,6 +29,12 @@ ActiveRecord::Schema.define(:version => 20130811112840) do
     t.boolean  "active"
     t.datetime "created_at",     :null => false
     t.datetime "updated_at",     :null => false
+  end
+
+  create_table "roles", :force => true do |t|
+    t.string   "title"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
   create_table "users", :force => true do |t|
