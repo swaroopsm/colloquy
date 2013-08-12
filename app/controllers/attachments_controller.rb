359482729109f35cfe8804ad7f87d6e125a1200c
Swaroop SM <swaroop.striker@gmail.com>
2013-08-12 17:00:45 +0530

    defined destroy action for attachment controller

--- app/controllers/attachments_controller.rb
+++ app/controllers/attachments_controller.rb
@@ -5,8 +5,14 @@ class AttachmentsController < ApplicationController
 	def index
 		@attachments = Attachment.includes(:attachable).order("attachable_id").all
 		@workshops = @attachments.select{ |a| a.attachable_type == "Workshop" }.group_by{ |id| id[:attachable_id] }
-		@plenaries = @attachments.select{ |a| a.attachable_type == "Plenary" }..group_by{ |id| id[:attachable_id] }
-		@pages = @attachments.select{ |a| a.attachable_type == "Page" }..group_by{ |id| id[:attachable_id] }
+		@plenaries = @attachments.select{ |a| a.attachable_type == "Plenary" }.group_by{ |id| id[:attachable_id] }
+		@pages = @attachments.select{ |a| a.attachable_type == "Page" }.group_by{ |id| id[:attachable_id] }
+	end
+
+	def destroy
+		@attachment = Attachment.find(params[:id])
+		@attachment.destroy
+		@attachment.pic.clear
 	end
 
 end
