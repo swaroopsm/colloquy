b0825766c70538c7ecdbe2757582567bf6169c8b
Swaroop SM <swaroop.striker@gmail.com>
2013-08-13 20:00:21 +0530

    added reviewer_check validation in reviewer submission model

--- app/models/reviewer_submission.rb
+++ app/models/reviewer_submission.rb
@@ -3,4 +3,11 @@ class ReviewerSubmission < ActiveRecord::Base
   belongs_to :user
   belongs_to :submission
 
+  validate :reviewer_check, :on => :create
+
+  private
+  def reviewer_check
+  	errors.add(:base, "You can only assign a reviewer to an abstract") unless self.user.reviewer?
+  end
+
 end
