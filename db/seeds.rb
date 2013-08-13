c7b548137b7f3f8ce30b758536300033a4053d11
gonecase <arjuna.shankar@gmail.com>
2013-08-13 13:30:27 +0530

    added seed for the roles model

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+roles = ["admin", "reviewer", "attendee"]
+
+roles.each do |r|
+	Role.create(title: r)
+end
