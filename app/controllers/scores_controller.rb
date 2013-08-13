7f4030b8db8e2135c5aa31edc58a7d610fc28c62
Swaroop SM <swaroop.striker@gmail.com>
2013-08-13 20:32:54 +0530

    defined create action in scores controller

--- app/controllers/scores_controller.rb
+++ app/controllers/scores_controller.rb
@@ -1,2 +1,13 @@
 class ScoresController < ApplicationController
+
+	before_filter :authenticate_user!
+	load_and_authorize_resource
+
+	def create
+		@score = Score.new(params[:score])
+		@score.submission = Submission.find(params[:submission_id])
+		@score.user = current_user
+		@score.save
+	end
+
 end
