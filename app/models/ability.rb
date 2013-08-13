41d809e1436b53678a332a429c8943554a2e4df7
Swaroop SM <swaroop.striker@gmail.com>
2013-08-13 14:57:38 +0530

    used a boolean method on the user object to check role in ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,9 +7,9 @@ class Ability
 # handling user privs
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
     user ||= User.new # guest user (not logged in)
-    user.role ||= Role.new 
+    user.role ||= Role.new
 
-    if user.role.title == "admin"
+    if user.admin?
          can :manage, Conference
          can :manage, Page
          can :manage, Pagecat
@@ -29,12 +29,12 @@ class Ability
     #     can :read, :all
     #   end
     #
-    # The first argument to `can` is the action you are giving the user 
+    # The first argument to `can` is the action you are giving the user
     # permission to do.
     # If you pass :manage it will apply to every action. Other common actions
     # here are :read, :create, :update and :destroy.
     #
-    # The second argument is the resource the user can perform the action on. 
+    # The second argument is the resource the user can perform the action on.
     # If you pass :all it will apply to every resource. Otherwise pass a Ruby
     # class of the resource.
     #
