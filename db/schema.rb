029db212fd1128fce656eec62c6cfd2017a8d335
gonecase <arjuna.shankar@gmail.com>
2013-08-13 15:14:58 +0530

    integrated friendly id and nesting for plenaries completely

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130813091553) do
+ActiveRecord::Schema.define(:version => 20130813094243) do
 
   create_table "attachments", :force => true do |t|
     t.string   "attachable_type"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(:version => 20130813091553) do
     t.integer  "user_id"
     t.datetime "created_at",    :null => false
     t.datetime "updated_at",    :null => false
+    t.string   "slug"
   end
 
   create_table "roles", :force => true do |t|
