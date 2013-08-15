db932b3055401aa33db859026187277a47e826d4
Swaroop SM <swaroop.striker@gmail.com>
2013-08-15 13:27:04 +0530

    added user_id to details and ran migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130815075528) do
+ActiveRecord::Schema.define(:version => 20130815075615) do
 
   create_table "attachments", :force => true do |t|
     t.string   "attachable_type"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(:version => 20130815075528) do
     t.string   "passport"
     t.datetime "created_at",            :null => false
     t.datetime "updated_at",            :null => false
+    t.integer  "user_id"
   end
 
   create_table "pagecats", :force => true do |t|
