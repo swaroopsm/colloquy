1381fd9065c872362b4671feca19331932fecc33
Swaroop SM <swaroop.striker@gmail.com>
2013-08-20 19:29:26 +0530

    added slug to topics and ran migrations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130915083645) do
+ActiveRecord::Schema.define(:version => 20130915083646) do
 
   create_table "attachments", :force => true do |t|
     t.string   "attachable_type"
@@ -163,7 +163,10 @@ ActiveRecord::Schema.define(:version => 20130915083645) do
     t.integer  "conference_id"
     t.datetime "created_at",    :null => false
     t.datetime "updated_at",    :null => false
+    t.string   "slug"
   end
+
+  add_index "topics", ["slug"], :name => "index_topics_on_slug", :unique => true
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
