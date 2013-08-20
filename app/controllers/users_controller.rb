e873140c20a001a7d84d30825e10f973b8b0ff4f
Swaroop SM <swaroop.striker@gmail.com>
2013-08-20 22:56:52 +0530

    modified participate action in users controller to accept user who has been inserted as well

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,10 +6,11 @@ class UsersController < ApplicationController
 		cur_ticket = params[:participate][:ticket]
 		cur_email = params[:participate][:email]
 		cur_user = nil
+		cur_conference = Conference.active
 		sync['participants'].each do |s|
 			r = ConferenceUser.new
 			r.ticket = s['Ticket_Number']
-			r.conference = Conference.active
+			r.conference = cur_conference
 			tickets << r
 			if s['Ticket_Number'] == cur_ticket and s['Email'] == cur_email
 				cur_user = s
@@ -21,33 +22,43 @@ class UsersController < ApplicationController
 		@user = User.new
 		unless cur_user.nil?
 			if params[:participate][:email] == cur_user["Email"] and params[:participate][:ticket] == cur_user["Ticket_Number"]
-				@user.email = cur_user["Email"]
-				@user.password = Devise.friendly_token[1..8]
-				@user.first_name = cur_user["participant_information"][0]["info"]
-				@user.last_name = cur_user["participant_information"][2]["info"]
-				if @user.save
+				query_user = User.find_by_email(params[:participate][:email])
+				if query_user.nil?
+					@user.email = cur_user["Email"]
+					@user.password = Devise.friendly_token[1..8]
+					@user.first_name = cur_user["participant_information"][0]["info"]
+					@user.last_name = cur_user["participant_information"][2]["info"]
+					if @user.save
 
-					# Register user for the conference
-					c = ConferenceUser.find_by_ticket(params[:participate][:ticket])
-					c.user = @user
-					c.save
+						# Register user for the conference
+						ConferenceUser.find_by_ticket(params[:participate][:ticket])
+						c.user = @user
+						c.save
 
-					# Populate User Detail
-					@detail = Detail.new
-					@detail.gender = cur_user["participant_information"][1]["info"]
-					@detail.dob = cur_user["participant_information"][3]["info"]
-					@detail.academic = cur_user["participant_information"][4]["info"]
-					@detail.institute_affiliation = cur_user["participant_information"][5]["info"]
-					@detail.address = cur_user["participant_information"][7]["info"]
-					@detail.phone = cur_user["participant_information"][6]["info"]
-					@detail.nationality = cur_user["participant_information"][8]["info"]
-					@detail.user = @user
-					@detail.save
+						# Populate User Detail
+						@detail = Detail.new
+						@detail.gender = cur_user["participant_information"][1]["info"]
+						@detail.dob = cur_user["participant_information"][3]["info"]
+						@detail.academic = cur_user["participant_information"][4]["info"]
+						@detail.institute_affiliation = cur_user["participant_information"][5]["info"]
+						@detail.address = cur_user["participant_information"][7]["info"]
+						@detail.phone = cur_user["participant_information"][6]["info"]
+						@detail.nationality = cur_user["participant_information"][8]["info"]
+						@detail.user = @user
+						@detail.save
 
-					# Make sure the below line is uncommented when in production
-					@user.status = :new_attendee
-					@user.send_reset_password_instructions
-
+						# Make sure the below line is uncommented when in production
+						@user.status = :new_attendee
+						@user.send_reset_password_instructions
+					end
+				else
+					if query_user.reset_password_token.nil?
+						# Make sure the below line is uncommented when in production
+						query_user.status = :new_attendee
+						query_user.send_reset_password_instructions
+					else
+						@user.errors.add(:base, "You have already registered for participation.")
+					end
 				end
 			end
 		else
