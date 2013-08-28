2bfa1ec7d4233cb382f7c7df491f0ec50fa71739
Swaroop SM <swaroop.striker@gmail.com>
2013-08-28 20:28:50 +0530

    added propose idea link

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,6 +1,6 @@
 class IdeasController < ApplicationController
 
-	before_filter :authenticate_user!, :except => [:index. :show]
+	before_filter :authenticate_user!, :except => [:index, :show]
 	load_and_authorize_resource :board, :except => [:index, :show]
 	load_and_authorize_resource	:idea, :except => [:index, :show], :through => :board
 
