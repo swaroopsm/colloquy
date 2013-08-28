f10fa7fa5784606cfddf82d612fd5c14fb82d291
Swaroop SM <swaroop.striker@gmail.com>
2013-08-28 20:40:56 +0530

    defined new action for ideas

--- app/controllers/ideas_controller.rb
+++ app/controllers/ideas_controller.rb
@@ -1,7 +1,18 @@
 class IdeasController < ApplicationController
 
 	before_filter :authenticate_user!, :except => [:index, :show]
-	load_and_authorize_resource :board, :except => [:index, :show]
-	load_and_authorize_resource	:idea, :except => [:index, :show], :through => :board
+	before_filter :load_board
+	load_and_authorize_resource	:idea, :except => [:index, :show]
+
+	def new
+		respond_to do |format|
+			format.js
+		end
+	end
+
+	private
+	def load_board
+		@board = Board.find(params[:board_id])
+	end
 
 end
