44dee36e12d134f375f313b34fe35ef646b7f344
Swaroop SM <swaroop.striker@gmail.com>
2013-08-28 18:35:09 +0530

    added after sign in path in applocation controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  def after_sign_in_path_for(resource)
+		if current_user.attendee?
+			attendee_index_path
+		else
+			root_path
+		end
+  end
+
 end
