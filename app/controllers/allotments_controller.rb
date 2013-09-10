91fce825ef0d1ee64f195c6a6bf2d5449dcaac4b
Swaroop SM <swaroop.striker@gmail.com>
2013-09-10 19:36:15 +0530

    defined new action for allotments controller

--- app/controllers/allotments_controller.rb
+++ app/controllers/allotments_controller.rb
@@ -1,2 +1,15 @@
 class AllotmentsController < ApplicationController
+
+	before_filter	:authenticate_user!
+	load_and_authorize_resource
+
+	def new
+		@periods = Period.all
+		@submission = Submission.find(params[:submission_id])
+
+		respond_to do |format|
+			format.js
+		end
+	end
+
 end
