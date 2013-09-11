d3f22b556f6a11519952e315afb5e644e12bccdc
Swaroop SM <swaroop.striker@gmail.com>
2013-09-11 12:01:14 +0530

    validation for uniqueness of allotment

--- app/models/allotment.rb
+++ app/models/allotment.rb
@@ -1,8 +1,10 @@
 class Allotment < ActiveRecord::Base
 	
 	attr_accessible	:period_id
+	
+	validates_uniqueness_of :period_id, :scope => [:allotable_id, :allotable_type], :message => "Abstract already assigned to Session"
 
 	belongs_to	:period
 	belongs_to	:allotable, :polymorphic => true
-
+	
 end
