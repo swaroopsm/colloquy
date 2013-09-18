0043056c26e1ec4655b773e2a09e0ec2841fb3fe
Swaroop SM <swaroop.striker@gmail.com>
2013-09-18 13:48:53 +0530

    model validation for schedule

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -2,5 +2,17 @@ class Schedule < ActiveRecord::Base
   attr_accessible :minutes, :starttime, :venue, :day
 
   belongs_to :schedulable, :polymorphic => true
+	
+	validates_presence_of	:day
+	validates_presence_of	:starttime
+	validates_presence_of	:minutes
+	validates_presence_of	:venue
+	validate :duplicate, :on => :create
+
+	private
+	def duplicate
+		model_name = self.schedulable_type == 'Period' ? "Session" : self.schedulable_type
+		errors.add(:base, "You have already scheduled this #{model_name} for that day") if Schedule.exists? :schedulable_id => self.schedulable_id, :schedulable_type => self.schedulable_type, :day => self.day
+	end
 
 end
