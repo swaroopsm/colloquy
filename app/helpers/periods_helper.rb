8442aacd14a14d18463e7d19c2dd86e178714a22
Swaroop SM <swaroop.striker@gmail.com>
2013-09-21 22:11:04 +0530

    defined period categories helper

--- app/helpers/periods_helper.rb
+++ app/helpers/periods_helper.rb
@@ -1,2 +1,7 @@
 module PeriodsHelper
+
+	def period_categories
+		{ "Normal Session" => Period::NORMAL_SESSION, "Special Session" => Period::SPECIAL_SESSION, "Break" => Period::BREAK_SESSION }
+	end
+
 end
