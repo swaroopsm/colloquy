4b7124edd79cf15fb53832d27386f224efbb0654
Swaroop SM <swaroop.striker@gmail.com>
2013-09-21 22:35:26 +0530

    added period_category helper and also display category field in sessions table

--- app/helpers/periods_helper.rb
+++ app/helpers/periods_helper.rb
@@ -4,4 +4,16 @@ module PeriodsHelper
 		{ "Normal Session" => Period::NORMAL_SESSION, "Special Session" => Period::SPECIAL_SESSION, "Break" => Period::BREAK_SESSION }
 	end
 
+	def period_category(cat)
+		if cat == Period::NORMAL_SESSION
+			"Normal Session"
+		elsif cat == Period::SPECIAL_SESSION
+			"Special Session"
+		elsif cat == Period::BREAK_SESSION
+			"Break"
+		else
+			"Normal Session"
+		end
+	end
+
 end
