748b36e5388b55fe9ac8d80c0ff7927ee23a8591
Swaroop SM <swaroop.striker@gmail.com>
2013-09-21 22:10:45 +0530

    defined session categories contants in period model

--- app/models/period.rb
+++ app/models/period.rb
@@ -1,6 +1,10 @@
 class Period < ActiveRecord::Base
   
-  attr_accessible :title
+  attr_accessible :title, :category, :content
+	
+	NORMAL_SESSION = 1
+	SPECIAL_SESSION = 2
+	BREAK_SESSION = 3
 
 	belongs_to	:conference
 	has_many		:submissions, :class_name => "Allotment", :foreign_key => :period_id
